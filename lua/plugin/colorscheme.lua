d7b6c1437b53ff8e6aa01aede919cad1ac214a6c
Swandono <me@swandono.com>
2023-04-29 21:52:58 +0700

    fix l map

--- lua/plugin/colorscheme.lua
+++ lua/plugin/colorscheme.lua
@@ -6,27 +6,75 @@ return {
         config = function()
             vim.cmd([[colorscheme gruvbox]])
         end,
+        keys = {
+            {
+                "<leader>cg",
+                function()
+                    vim.cmd([[colorscheme gruvbox]])
+                end,
+            },
+        },
     },
     {
         "folke/tokyonight.nvim",
         lazy = true,
+        keys = {
+            {
+                "<leader>ct",
+                function()
+                    vim.cmd([[colorscheme tokyonight]])
+                end,
+            },
+        },
         opts = { style = "moon" },
     },
     {
         "catppuccin/nvim",
         lazy = true,
+        keys = {
+            {
+                "<leader>cc",
+                function()
+                    vim.cmd([[colorscheme catppuccin]])
+                end,
+            },
+        },
         name = "catppuccin",
     },
     {
         "rmehri01/onenord.nvim",
         lazy = true,
+        keys = {
+            {
+                "<leader>co",
+                function()
+                    vim.cmd([[colorscheme onenord]])
+                end,
+            },
+        },
     },
     {
         "shaunsingh/moonlight.nvim",
         lazy = true,
+        keys = {
+            {
+                "<leader>cm",
+                function()
+                    vim.cmd([[colorscheme moonlight]])
+                end,
+            },
+        },
     },
     {
         "glepnir/zephyr-nvim",
         lazy = true,
+        keys = {
+            {
+                "<leader>cz",
+                function()
+                    vim.cmd([[colorscheme zephyr]])
+                end,
+            },
+        },
     },
 }
