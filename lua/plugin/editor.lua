2c86a1e22b2538c9e0238800a3ba4144a8bc553b
Swandono <me@swandono.com>
2023-04-29 16:08:58 +0700

    change fileexplorer plugin

--- lua/plugin/editor.lua
+++ lua/plugin/editor.lua
@@ -1,34 +1,126 @@
 local A = {
-    {
-        "folke/trouble.nvim",
-        cmd = { "TroubleToggle", "Trouble" },
-        opts = { use_diagnostic_signs = true },
-        keys = {
-            { "<leader>ll", "<cmd>TroubleToggle<cr>" },
-            { "<leader>ld", "<cmd>TroubleToggle document_diagnostics<cr>",  desc = "Document Diagnostics (Trouble)" },
-            { "<leader>lw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
-            { "<leader>lc", "<cmd>TroubleToggle loclist<cr>",               desc = "Location List (Trouble)" },
-            { "<leader>lq", "<cmd>TroubleToggle quickfix<cr>",              desc = "Quickfix List (Trouble)" },
-            {
-                "lp",
-                function()
-                    if require("trouble").is_open() then
-                        require("trouble").previous({ skip_groups = true, jump = true })
-                    end
-                end,
-                desc = "Previous trouble/quickfix item",
-            },
-            {
-                "ln",
-                function()
-                    if require("trouble").is_open() then
-                        require("trouble").next({ skip_groups = true, jump = true })
-                    end
-                end,
-                desc = "Next trouble/quickfix item",
-            },
-        },
-    }
+	{
+		"folke/trouble.nvim",
+		cmd = { "TroubleToggle", "Trouble" },
+		opts = { use_diagnostic_signs = true },
+		keys = {
+			{ "<leader>ll", "<cmd>TroubleToggle<cr>" },
+			{ "<leader>ld", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
+			{ "<leader>lw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
+			{ "<leader>lc", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
+			{ "<leader>lq", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
+			{
+				"lp",
+				function()
+					if require("trouble").is_open() then
+						require("trouble").previous({ skip_groups = true, jump = true })
+					end
+				end,
+				desc = "Previous trouble/quickfix item",
+			},
+			{
+				"ln",
+				function()
+					if require("trouble").is_open() then
+						require("trouble").next({ skip_groups = true, jump = true })
+					end
+				end,
+				desc = "Next trouble/quickfix item",
+			},
+		},
+	},
 }
 
-return { A }
+local B = {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+        event = "VeryLazy",
+		cmd = "Neotree",
+		dependencies = {
+			{ "MunifTanjim/nui.nvim", lazy = true },
+		},
+		keys = {
+			{
+				"<C-h>",
+				function()
+					require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+				end,
+				desc = "Explorer NeoTree (cwd)",
+			},
+			{ "<leader>rt", "<C-h>", desc = "Explorer NeoTree (cwd)", remap = true },
+		},
+		deactivate = function()
+			vim.cmd([[Neotree close]])
+		end,
+		init = function()
+			vim.g.neo_tree_remove_legacy_commands = 1
+			if vim.fn.argc() == 1 then
+				local stat = vim.loop.fs_stat(vim.fn.argv(0))
+				if stat and stat.type == "directory" then
+					require("neo-tree")
+				end
+			end
+		end,
+		opts = {
+			filesystem = {
+				bind_to_cwd = false,
+				follow_current_file = true,
+				use_libuv_file_watcher = true,
+			},
+			window = {
+				mappings = {
+					["<space>"] = "none",
+				},
+			},
+			default_component_configs = {
+				indent = {
+					with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+					expander_collapsed = "",
+					expander_expanded = "",
+					expander_highlight = "NeoTreeExpander",
+				},
+			},
+		},
+		config = function(_, opts)
+			require("neo-tree").setup(opts)
+			vim.api.nvim_create_autocmd("TermClose", {
+				pattern = "*lazygit",
+				callback = function()
+					if package.loaded["neo-tree.sources.git_status"] then
+						require("neo-tree.sources.git_status").refresh()
+					end
+				end,
+			})
+		end,
+	},
+}
+
+local C = {
+	{
+		"nvim-pack/nvim-spectre",
+        -- stylua: ignore
+        keys = {
+            { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
+        },
+	},
+}
+
+local D = {
+	{
+		"folke/todo-comments.nvim",
+		cmd = { "TodoTrouble", "TodoTelescope" },
+		event = { "BufReadPost", "BufNewFile" },
+		config = true,
+        -- stylua: ignore
+        keys = {
+            { "<leader>tn", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+            { "<leader>tp", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+            { "<leader>tt", "<cmd>TodoTrouble<cr>",                              desc = "Todo (Trouble)" },
+            { "<leader>tT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>",      desc = "Todo/Fix/Fixme (Trouble)" },
+            { "<leader>ts", "<cmd>TodoTelescope<cr>",                            desc = "Todo" },
+            { "<leader>tS", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>",    desc = "Todo/Fix/Fixme" },
+        },
+	},
+}
+
+return { A, B, C, D }
--- lua/plugin/ui.lua
+++ lua/plugin/ui.lua
@@ -1,88 +1,73 @@
 local A = {
-    'nvim-lualine/lualine.nvim',
-    event = "VeryLazy",
-    opts = {
-        options = {
-            icons_enabled = true,
-            theme = 'auto',
-            component_separators = { left = '', right = '' },
-            section_separators = { left = '', right = '' },
-            disabled_filetypes = {},
-            always_divide_middle = true,
-            globalstatus = true,
-        },
-        sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch', 'diff', 'diagnostics' },
-            lualine_c = { { 'filename', path = 1 } },
-            lualine_x = { 'encoding', 'fileformat', 'filetype' },
-            lualine_y = { 'progress' },
-            lualine_z = { 'location' }
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = { 'filename' },
-            lualine_x = { 'location' },
-            lualine_y = {},
-            lualine_z = {}
-        }
-    }
+	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "auto",
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
+			disabled_filetypes = {},
+			always_divide_middle = true,
+			globalstatus = true,
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = { { "filename", path = 1 } },
+			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = { "filename" },
+			lualine_x = { "location" },
+			lualine_y = {},
+			lualine_z = {},
+		},
+	},
 }
 local B = {
-    'akinsho/bufferline.nvim',
-    event = "VeryLazy",
-    opts = {
-        options = {
+	"akinsho/bufferline.nvim",
+	event = "VeryLazy",
+	opts = {
+		options = {
             -- stylua: ignore
             close_command = function(n) require("mini.bufremove").delete(n, false) end,
             -- stylua: ignore
             right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
-            diagnostics = "nvim_lsp",
-            always_show_bufferline = true,
-        },
-    },
+			diagnostics = "nvim_lsp",
+			always_show_bufferline = true,
+		},
+	},
 }
 local C = {
-    'lukas-reineke/indent-blankline.nvim',
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-        -- char = "▏",
-        char = "│",
-        filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-        show_trailing_blankline_indent = false,
-        show_current_context = false,
-    },
+	"lukas-reineke/indent-blankline.nvim",
+	event = { "BufReadPost", "BufNewFile" },
+	opts = {
+		-- char = "▏",
+		char = "│",
+		filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+		show_trailing_blankline_indent = false,
+		show_current_context = false,
+	},
 }
 local D = {
-    'yamatsum/nvim-cursorline',
-    event = { "BufReadPre", "BufNewFile", "InsertEnter" },
-    opts = {
-        cursorline = {
-            enable = true,
-            timeout = 0,
-            number = false,
-        },
-        cursorword = {
-            enable = false
-        }
-    }
+	"yamatsum/nvim-cursorline",
+	event = { "BufReadPre", "BufNewFile", "InsertEnter" },
+	opts = {
+		cursorline = {
+			enable = true,
+			timeout = 0,
+			number = false,
+		},
+		cursorword = {
+			enable = false,
+		},
+	},
 }
-local E = {
-    'kyazdani42/nvim-tree.lua',
-    event = "VeryLazy",
-    cmd = "NvimTreeToggle",
-    opts = {
-        view = {
-            width = 50
-        }
-    },
-    config = function(_, opts)
-        require("nvim-tree").setup(opts)
-        vim.keymap.set("n", "<leader>rt", [[<CMD>NvimTreeRefresh<CR>]])
-        vim.keymap.set("n", "<C-h>", [[<CMD>NvimTreeToggle<CR>]])
-    end
-}
-local F = { 'kyazdani42/nvim-web-devicons', lazy = true }
+local E = { "kyazdani42/nvim-web-devicons", lazy = true }
 
-return { A, B, C, D, E, F }
+return { A, B, C, D, E }
