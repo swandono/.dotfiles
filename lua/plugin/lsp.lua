bbac258f2b0280803b3bf7caa24edf936fc76556
Swandono <me@swandono.com>
2023-05-01 10:39:10 +0700

    add formatter

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -1,18 +1,18 @@
 return {
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v2.x",
         event = { "BufReadPre", "BufNewFile", "InsertEnter" },
         dependencies = {
-            { 'neovim/nvim-lspconfig' }, -- Required
+            { "neovim/nvim-lspconfig" }, -- Required
             {
                 -- Optional
-                'williamboman/mason.nvim',
+                "williamboman/mason.nvim",
                 build = function()
-                    pcall(vim.cmd, 'MasonUpdate')
+                    pcall(vim.cmd, "MasonUpdate")
                 end,
             },
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            { "williamboman/mason-lspconfig.nvim" }, -- Optional
         },
         opts = function()
             local lsp = require("lsp-zero")
@@ -20,9 +20,9 @@ return {
             lsp.preset("recommended")
 
             lsp.ensure_installed({
-                'tsserver',
-                'rust_analyzer',
-                'gopls'
+                "tsserver",
+                "rust_analyzer",
+                "gopls",
             })
 
             -- Fix Undefined global 'vim'
@@ -31,53 +31,121 @@ return {
             lsp.set_preferences({
                 suggest_lsp_servers = false,
                 sign_icons = {
-                    error = 'E',
-                    warn = 'W',
-                    hint = 'H',
-                    info = 'I'
-                }
+                    error = "E",
+                    warn = "W",
+                    hint = "H",
+                    info = "I",
+                },
             })
 
             lsp.on_attach(function(client, bufnr)
                 local opts = { buffer = bufnr, remap = false }
 
-                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition {} end, opts)
-                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.hover() end, opts)
-                vim.keymap.set("n", "<leader>ff", function() vim.lsp.buf.format { async = true } end, opts)
-                vim.keymap.set("n", "<leader>va", function() vim.lsp.buf.code_action() end, opts)
-                vim.keymap.set("n", "<leader>vf", function() vim.diagnostic.open_float() end, opts)
-                vim.keymap.set("n", "<leader>vh", function() vim.lsp.buf.hover() end, opts)
-                vim.keymap.set("n", "<leader>vn", function() vim.diagnostic.goto_next() end, opts)
-                vim.keymap.set("n", "<leader>vp", function() vim.diagnostic.goto_prev() end, opts)
-                vim.keymap.set("n", "<leader>vr", function() vim.lsp.buf.rename() end, opts)
-                vim.keymap.set("n", "<leader>vs", function() vim.lsp.buf.signature_help() end, opts)
+                vim.keymap.set("n", "gd", function()
+                    vim.lsp.buf.definition({})
+                end, opts)
+                vim.keymap.set("i", "<C-h>", function()
+                    vim.lsp.buf.hover()
+                end, opts)
+                vim.keymap.set("n", "<leader>ff", function()
+                    vim.lsp.buf.format({ async = true })
+                end, opts)
+                vim.keymap.set("n", "<leader>va", function()
+                    vim.lsp.buf.code_action()
+                end, opts)
+                vim.keymap.set("n", "<leader>vf", function()
+                    vim.diagnostic.open_float()
+                end, opts)
+                vim.keymap.set("n", "<leader>vh", function()
+                    vim.lsp.buf.hover()
+                end, opts)
+                vim.keymap.set("n", "<leader>vn", function()
+                    vim.diagnostic.goto_next()
+                end, opts)
+                vim.keymap.set("n", "<leader>vp", function()
+                    vim.diagnostic.goto_prev()
+                end, opts)
+                vim.keymap.set("n", "<leader>vr", function()
+                    vim.lsp.buf.rename()
+                end, opts)
+                vim.keymap.set("n", "<leader>vs", function()
+                    vim.lsp.buf.signature_help()
+                end, opts)
             end)
 
             lsp.setup()
 
             vim.diagnostic.config({
-                virtual_text = true
+                virtual_text = true,
             })
-        end
-    },
-    -- formatters
-    {
-        "jose-elias-alvarez/null-ls.nvim",
-        event = { "BufReadPre", "BufNewFile", "InsertEnter" },
-        dependencies = { "mason.nvim" },
-        opts = function()
-            local nls = require("null-ls")
-            return {
-                root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-                sources = {
-                    nls.builtins.formatting.fish_indent,
-                    nls.builtins.diagnostics.fish,
-                    nls.builtins.formatting.stylua,
-                    nls.builtins.formatting.shfmt,
-                    -- nls.builtins.diagnostics.flake8,
-                },
-            }
         end,
     },
-
+    {
+        "mhartington/formatter.nvim",
+        event = { "BufReadPre", "BufNewFile", "InsertEnter" },
+        config = function(_, opts)
+            local function prettier()
+                return {
+                    exe = "prettierd",
+                    args = { vim.api.nvim_buf_get_name(0) },
+                    stdin = true
+                }
+            end
+            local function go()
+                return {
+                    exe = "gofmt",
+                    stdin = true,
+                }
+            end
+            local function rust()
+                return {
+                    exe = "rustfmt",
+                    stdin = true,
+                }
+            end
+            local function python()
+                return {
+                    exe = "black",
+                    args = {
+                        '-q',
+                        '-',
+                    },
+                    stdin = true,
+                }
+            end
+            local function java()
+                return {
+                    exe = "clang-format",
+                    args = { "--style=Google", "--assume-filename=.java" },
+                    stdin = true
+                }
+            end
+            require("formatter").setup({
+                logging = true,
+                filetype = {
+                    javascript = { prettier },
+                    javascriptreact = { prettier },
+                    typescript = { prettier },
+                    typescriptreact = { prettier },
+                    css = { prettier },
+                    scss = { prettier },
+                    vue = { prettier },
+                    json = { prettier },
+                    jsonc = { prettier },
+                    yaml = { prettier },
+                    markdown = { prettier },
+                    html = { prettier },
+                    go = { go },
+                    rust = { rust },
+                    python = { python },
+                    java = { java }
+                },
+                ["*"] = {
+                    require("formatter.filetypes.any").remove_trailing_whitespace
+                }
+            })
+            vim.keymap.set("n", "<leader>fF", [[<CMD> Format<CR>]])
+            vim.keymap.set("n", "<leader>fw", [[<CMD> FormatWrite<CR>]])
+        end,
+    },
 }
--- lua/swandono/remap.lua
+++ lua/swandono/remap.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>fs", vim.cmd.Ex)
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
