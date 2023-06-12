2fa40d0fb8efa2bbbfb241552e7575ea627aea00
Swandono <me@swandono.com>
2023-06-12 19:41:48 +0700

    remove dap not using it

--- lua/swandono/remap.lua
+++ lua/swandono/remap.lua
@@ -37,7 +37,6 @@ vim.keymap.set("n", "<leader>ws]", [[<CMD>vertical resize +5<CR>]])
 vim.keymap.set("n", "<leader>ws;", [[<CMD>resize -5<CR>]])
 vim.keymap.set("n", "<leader>ws'", [[<CMD>resize +5<CR>]])
 
-vim.keymap.set("n", "<leader>gd", vim.cmd.Gdif)
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gb", [[<CMD> Git blame<CR>]], { silent = true, noremap = true })
 vim.keymap.set("n", "<leader>gj", [[<CMD> Neogit kind=replace<CR>]], { silent = true, noremap = true })
