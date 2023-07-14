01bc9cbdb5e2501eb0d7b47bbff26d2ec3a74aa0
Swandono <me@swandono.com>
2023-07-14 17:03:59 +0700

    restructure remap for debugging

--- lua/plugin/dap.lua
+++ lua/plugin/dap.lua
@@ -3,10 +3,6 @@ return {
     dependencies = {
         {
             "rcarriga/nvim-dap-ui",
-            keys = {
-                { "<leader>dd", function() require("dapui").toggle({}) end,    desc = "Toggle DAP UI" },
-                { "<leader>dr", function() require("dap").repl.toggle({}) end, desc = "Toggle REPL" },
-            },
             opts = {},
             config = function(_, opts)
                 local dap = require("dap")
@@ -65,66 +61,22 @@ return {
         },
     },
     keys = {
-        {
-            "<leader>dj",
-            function() require("dap").toggle_breakpoint() end,
-            desc =
-            "Toggle Breakpoint"
-        },
-        {
-            "<leader>dc",
-            function() require("dap").continue() end,
-            desc =
-            "Continue"
-        },
-        {
-            "<leader>dp",
-            function() require("dap").down() end,
-            desc =
-            "Down"
-        },
-        { "<leader>dn", function() require("dap").up() end, desc = "Up" },
-        {
-            "<leader>dl",
-            function() require("dapui").eval() end,
-            desc =
-            "Eval"
-        },
-        {
-            "<leader>dt",
-            function() require("dap").terminate() end,
-            desc =
-            "Terminate"
-        },
-        {
-            "<leader>dh",
-            function() require("dap.ui.widgets").hover() end,
-            desc =
-            "Widgets"
-        },
-        {
-            "<leader>dk",
-            function() require("dap").run_to_cursor() end,
-            desc =
-            "Go to line (no execute)"
-        },
-        {
-            "<leader>di",
-            function() require("dap").step_into() end,
-            desc =
-            "Step Into"
-        },
-        {
-            "<leader>do",
-            function() require("dap").step_out() end,
-            desc =
-            "Step Out"
-        },
-        {
-            "<leader>dO",
-            function() require("dap").step_over() end,
-            desc =
-            "Step Over"
-        },
+        { "<leader>da", function() require("dap").toggle_breakpoint() end,            desc = "Toggle Breakpoint" },
+        { "<leader>dc", function() require("dap").continue() end,                     desc = "Continue" },
+        { "<leader>df", function() require("dap").run_to_cursor() end,                desc = "Go to line (no execute)" },
+        { "<leader>dO", function() require("dap").step_over() end,                    desc = "Step Over" },
+        { "<leader>di", function() require("dap").step_into() end,                    desc = "Step Into" },
+        { "<leader>do", function() require("dap").step_out() end,                     desc = "Step Out" },
+        { "<leader>dh", function() require("dap.ui.widgets").hover() end,             desc = "Widgets" },
+        { "<leader>dd", function() require("dapui").eval() end,                       desc = "Debug Eval" },
+        { "<leader>dt", function() require("dapui").toggle({}) end,                   desc = "Toggle DAP UI" },
+        { "<leader>db", function() require("dapui").float_element("breakpoints") end, desc = "Debug Breakpoints" },
+        { "<leader>d;", function() require("dapui").float_element("watches") end,     desc = "Debug Watches" },
+        { "<leader>dj", function() require("dapui").float_element("scopes") end,      desc = "Debug Scopes" },
+        { "<leader>dk", function() require("dapui").float_element("repl") end,        desc = "Debug REPL" },
+        { "<leader>dl", function() require("dapui").float_element("console") end,     desc = "Debug Terminal" },
+        { "<leader>dm", function() require("dapui").float_element("stacks") end,      desc = "Debug Stacks" },
+        { "<leader>dn", function() require("dap").up() end,                           desc = "Up" },
+        { "<leader>dp", function() require("dap").down() end,                         desc = "Down" },
     },
 }
