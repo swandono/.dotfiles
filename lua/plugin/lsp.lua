963e6cacc587c504b7e97803775e1a64cdf63adb
Swandono <me@swandono.com>
2023-08-26 00:39:41 +0700

    change tsserver to vtsls

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -20,7 +20,7 @@ return {
             lsp.preset("recommended")
 
             lsp.ensure_installed({
-                "tsserver",
+                "vtsls",
                 "rust_analyzer",
                 "gopls",
             })
