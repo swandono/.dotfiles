7e2739baf52cb9b02bf60bda2b4983aa1f123e14
Gunawan Swandono <me@swandono.com>
2024-01-07 23:04:27 +0700

    feat: fix jdtls

--- lua/plugin/languages.lua
+++ lua/plugin/languages.lua
@@ -7,22 +7,21 @@ local A = {
     --     local config = {
     --         filetype = { 'java' },
     --         cmd = {
-    --             '/users/gunawan/.jenv/shims/java',
-    --             '-declipse.application=org.eclipse.jdt.ls.core.id1',
-    --             '-dosgi.bundles.defaultstartlevel=4',
-    --             '-declipse.product=org.eclipse.jdt.ls.core.product',
-    --             '-dlog.protocol=true',
-    --             '-dlog.level=all',
-    --             '-xms2g',
-    --             '-xmx2g',
-    --             '-javaagent:/users/gunawan/.local/share/jdtls/lombok.jar',
-    --             '-jar',
-    --             '/users/gunawan/.local/share/jdtls/plugins/org.eclipse.equinox.launcher_1.6.500.v20230622-2056.jar',
-    --             '-configuration', '/users/gunawan/.local/share/jdtls/config_mac',
-    --             '-data', '/users/gunawan/.local/share/jdtls/workspace/' .. project_name,
-    --             '--add-modules=all-system',
-    --             '--add-opens', 'java.base/java.util=all-unnamed',
-    --             '--add-opens', 'java.base/java.lang=all-unnamed',
+    --             '/Users/gunawan/.jenv/shims/java',
+    --             '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    --             '-Dosgi.bundles.defaultStartLevel=4',
+    --             '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    --             '-Dlog.protocol=true',
+    --             '-Dlog.level=ALL',
+    --             '-Xms2G',
+    --             '-Xmx2G',
+    --             '-javaagent:/Users/gunawan/.local/share/jdtls/lombok.jar',
+    --             '-jar', '/Users/gunawan/.local/share/jdtls/plugins/org.eclipse.equinox.launcher_1.6.500.v20230622-2056.jar',
+    --             '-configuration', '/Users/gunawan/.local/share/jdtls/config_mac',
+    --             '-data', '/Users/gunawan/.local/share/jdtls/workspace/' .. project_name,
+    --             '--add-modules=ALL-SYSTEM',
+    --             '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    --             '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
     --         },
     --         root_dir = require('jdtls.setup').find_root({ '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' }),
     --
@@ -30,15 +29,15 @@ local A = {
     --     config['init_options'] = {
     --         bundles = {
     --             vim.fn.glob(
-    --                 "/users/gunawan/.local/share/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.47.0.jar",
+    --                 "/Users/gunawan/.local/share/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.47.0.jar",
     --                 1)
     --         },
     --     }
     --     require('jdtls').start_or_attach(config)
-    --     vim.keymap.set("n", "<leader>dg", [[<cmd> jdtupdatedebugconfig<cr>]],
-    --         { silent = true, noremap = true, desc = "update debug config" })
-    --     vim.keymap.set("n", "<leader>dg", [[<cmd> jdtupdateconfig<cr>]],
-    --         { silent = true, noremap = true, desc = "update config" })
+    --     vim.keymap.set("n", "<leader>dg", [[<CMD> JdtUpdateDebugConfig<CR>]],
+    --         { silent = true, noremap = true, desc = "Update Debug Config" })
+    --     vim.keymap.set("n", "<leader>dG", [[<CMD> JdtUpdateConfig<CR>]],
+    --         { silent = true, noremap = true, desc = "Update Config" })
     -- end
 }
 
