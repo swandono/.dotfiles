10ed269c5003bb2817d4cd85d4f7a4c304c581a1
Swandono <me@swandono.com>
2024-02-29 21:17:43 +0700

    feat: change neotest keymaps

--- lua/plugin/test.lua
+++ lua/plugin/test.lua
@@ -10,9 +10,10 @@ return {
             "rcasia/neotest-java"
         },
         keys = {
-            { "<leader>rt" },
-            { "<leader>rT" },
-            { "<leader>rr" },
+            { "<leader>rj" },
+            { "<leader>rk" },
+            { "<leader>rl" },
+            { "<leader>ri" },
             { "<leader>ro" },
         },
         config = function()
@@ -24,19 +25,23 @@ return {
                     require("neotest-java"),
                 }
             })
-            vim.keymap.set("n", "<leader>rt", function()
+            vim.keymap.set("n", "<leader>rj", function()
                 neotest.run.run()
             end, { desc = "Run single function test" })
-            vim.keymap.set("n", "<leader>rT", function()
+            vim.keymap.set("n", "<leader>rk", function()
                 neotest.run.run(vim.fn.expand("%"))
             end, { desc = "Run file tests" })
-            vim.keymap.set("n", "<leader>rr", function()
+            vim.keymap.set("n", "<leader>rl", function()
+                neotest.output_panel.toggle()
                 neotest.summary.toggle()
-            end, { desc = "Open Neotest Summary" })
-            vim.keymap.set("n", "<leader>ro", function()
+            end, { desc = "Open Neotest Dashboard" })
+            vim.keymap.set("n", "<leader>ri", function()
                 neotest.output_panel.toggle()
             end, { desc = "Open Neotest Output Panel" })
-            vim.keymap.set("n", "<leader>rc", function()
+            vim.keymap.set("n", "<leader>ro", function()
+                neotest.summary.toggle()
+            end, { desc = "Open Neotest Summary" })
+            vim.keymap.set("n", "<leader>rm", function()
                 neotest.output_panel.clear()
             end, { desc = "Clear Neotest Output Panel" })
         end,
