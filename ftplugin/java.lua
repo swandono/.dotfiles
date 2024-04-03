1d406e08273274c8b0f95c91514953c6ae962f8f
Swandono <me@swandono.com>
2024-04-03 12:26:31 +0700

    feat: updating jenv to sdkman

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -3,7 +3,7 @@ local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 local config = {
 	filetype = { "java" },
 	cmd = {
-		"/Users/swandono/.jenv/shims/java",
+		"/Users/swandono/.sdkman/candidates/java/current/bin/java",
 		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
 		"-Dosgi.bundles.defaultStartLevel=4",
 		"-Declipse.product=org.eclipse.jdt.ls.core.product",
