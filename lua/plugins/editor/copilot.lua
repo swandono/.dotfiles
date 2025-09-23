1c65f9ced592694742b6a933ad144e593c61fbc7
Swandono <me@swandono.com>
2025-09-23 17:35:32 +0700

    remove: supermaven

--- lua/plugins/editor/copilot.lua
+++ lua/plugins/editor/copilot.lua
@@ -2,7 +2,7 @@ return {
 	"zbirenbaum/copilot.lua",
 	cmd = "Copilot",
 	build = ":Copilot auth",
-	event = { "BufReadPre", "BufNewFile", "InsertEnter" },
+	event = { "InsertEnter" },
 	requires = {
 		"copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
 	},
